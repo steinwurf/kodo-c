b5b8e07c11d000f10b20cfc948d66554fa6b6c3e
Peter Vingelmann <petya@master.com>
2016-09-22 00:19:22 +0200

    Fixes for obj-c headers

--- examples/ios_app/kodoc-ios-demo/kodoc-ios-demo/ViewController.h
+++ examples/ios_app/kodoc-ios-demo/kodoc-ios-demo/ViewController.h
@@ -9,8 +9,9 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+{}
 
-    @property(strong, nonatomic) IBOutlet UIButton* start_button;
+@property(strong, nonatomic) IBOutlet UIButton* start_button;
 @property(strong, nonatomic) IBOutlet UILabel* info_label;
 @property int counter;
 
